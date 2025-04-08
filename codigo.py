7054bea64697d4d71e9be8396d3a85a44db09ad7
LevyCostta <levicostasouza@hotmail.com>
2025-04-07 22:20:15 -0300

    Relizado PASSO 4 : Cadastrar um produto

--- codigo.py
+++ codigo.py
@@ -29,11 +29,44 @@ sleep(3)
 
 # PASSO 3 : Importrar a base de dados
 import pandas
-
 tabela = pandas.read_csv('produtos.csv')
-
+print(tabela)
 
 # PASSO 4 : Cadastrar um produto
+for linha in tabela.index:
+    pyautogui.click(x=415, y=251)
+
+    codigo = tabela.loc[linha, 'codigo']
+    pyautogui.write(codigo)
+    pyautogui.press('tab')
+
+    marca = tabela.loc[linha, 'marca']
+    pyautogui.write(marca)
+    pyautogui.press('tab')
+
+    tipo = tabela.loc[linha, 'tipo']
+    pyautogui.write(tipo)
+    pyautogui.press('tab')
+
+    categoria = str(tabela.loc[linha, 'categoria'])
+    pyautogui.write(categoria)
+    pyautogui.press('tab')
+
+    preco_unitario = str(tabela.loc[linha, 'preco_unitario'])
+    pyautogui.write(preco_unitario)
+    pyautogui.press('tab')
+
+    custo = str(tabela.loc[linha, 'custo'])
+    pyautogui.write(custo)
+    pyautogui.press('tab')
+
+    obs = str(tabela.loc[linha, 'obs'])
+    if obs != 'nan':
+        pyautogui.write(obs)
+
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+    pyautogui.scroll(10000)#voltar para o inicio da pagina
 # PASSO 5 : Repetir para todos os produtos
 
 
